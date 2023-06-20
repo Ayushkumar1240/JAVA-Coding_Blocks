974434a5a6b183ccd19d23af7b3290de8bf27baa
Ayush Kumar <ayushkumar4975@gmail.com>
2023-06-20 18:09:04 +0530

    making my day three commit

--- DayThree.java
+++ DayThree.java
@@ -1,39 +1,111 @@
+//      *** ***
+//      **   **
+//      *     *
+     
+//      *     *
+//      **   **
+//      *** ***
+
+
+
+// public class DayThree
+// {
+//     public static void main(String[] args) 
+//     {
+//         int n=7;
+//         int nst=n/2;
+//         int nsp=1;
+
+//         int row=0;
+
+//         while(row<n)
+//         {
+//             for(int i=0;i<nst;i++)
+//             {
+//                 System.out.print("*");
+//             }
+//             for(int i=0;i<nsp;i++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             for(int i=0;i<nst;i++)
+//             {
+//                 System.out.print("*");
+//             }
+//             if(row<n/2)
+//             {
+//                 nst--;
+//                 nsp+=2;
+//             }
+//             else
+//             {
+//                 nst++;
+//                 nsp-=2;
+//             }
+//             System.out.println();
+//             row++;
+//         }
+//     }
+// }
+
+
+
+
+
+
+//     *******
+//     *** ***
+//     **   **
+//     *     *
+//     **   **
+//     *** ***
+
 public class DayThree
 {
     public static void main(String[] args) 
     {
         int n=7;
-        int nst=n/2;
-        int nsp=1;
+        int nst1=(n/2)+1;
+        int nst2=n/2;
+        int nsp=-1;
 
         int row=0;
 
         while(row<n)
         {
-            for(int i=0;i<nst;i++)
+            for(int i=0;i<nst1;i++)
             {
-                System.out.print("* ");
+                System.out.print("*");
             }
             for(int i=0;i<nsp;i++)
             {
                 System.out.print(" ");
             }
-            for(int i=0;i<nst;i++)
+            if(row==n-1)
             {
-                System.out.print("* ");
+                nst2--;
             }
-            if(row<n/2)
+            for(int i=0;i<nst2;i++)
             {
-                nst--;
-                nsp++;
+                System.out.print("*");
+            }
+            if(row<(n/2))
+            {
+                nst1--;
+                if(row!=0)
+                {
+                    nst2--;
+                }
+                nsp+=2;
             }
             else
             {
-                nst++;
-                nsp--;
+                nst1++;
+                nst2++;
+                nsp-=2;
             }
             System.out.println();
-            row--;
+            row++;
         }
     }
 }
