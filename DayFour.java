d80729ef445a43da84de0b74d0456d2f683ffa78
Ayush Kumar <ayushkumar4975@gmail.com>
2023-06-21 15:53:49 +0530

    commiiting

---
+++ DayFour.java
@@ -0,0 +1,13 @@
+public class DayFour 
+{
+    public static void main(String[] args) 
+    {
+        int a=10;
+        int b=12;
+        // System.out.println(a++ + b++ - a-- + ++a);
+        // System.out.println(++a);
+        //  System.out.println(a--);
+        // System.out.println(--a);
+        System.out.println(--a + b-- - ++a + ++a);
+    }
+}
